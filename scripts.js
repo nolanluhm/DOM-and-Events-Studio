10ec5a8630059f15fbc0f75e7c7c5e57ad0ae749
nolanluhm <nolanluhm@gmail.com>
2020-03-18 21:20:20 -0500

    added button functionality

--- scripts.js
+++ scripts.js
@@ -2,17 +2,56 @@
 // Remember to pay attention to page loading!
 
 window.addEventListener("load", function () {
-    let rocketImg = document.getElementById("rocket");
-    let takeoffButton = document.getElementById("takeoff");
-    let shuttleStatus = document.getElementById("flightStatus")
+
 });
+
+let rocketImg = document.getElementById("rocket");
+    rocketImg.style.position = "absolute";
+    rocketImg.style.left = "0px";
+    rocketImg.style.bottom = "0px";
+
+let takeoffButton = document.getElementById("takeoff");
+let landButton = document.getElementById("landing");
+let abortMissionButton = document.getElementById("missionAbort");
+
+let upButton = document.getElementById("up");
+
+let shuttleStatus = document.getElementById("flightStatus");
+let shuttleBackground = document.getElementById("shuttleBackground");
+let shuttleHeight = document.getElementById("spaceShuttleHeight");
+
 
 takeoffButton.addEventListener("click", function() {
     let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
-
     if (result) {
-        flightStatus.innerHTML = "Shuttle in flight.”;
+
+        shuttleStatus.innerHTML = "Shuttle in flight.";
+        shuttleBackground.style.background = "blue";
+        let newShuttleHeight = (Number(shuttleHeight.innerHTML) + 10000);
+        shuttleHeight.innerHTML = "" + newShuttleHeight;
     }
 });
+
+landButton.addEventListener("click", function() {
+    window.alert("The shuttle is landing. Landing gear engaged.");
+    shuttleStatus.innerHTML = "The shuttle has landed.";
+    shuttleBackground.style.background = "green";
+    shuttleHeight.innerHTML = 0;
+});
+
+abortMissionButton.addEventListener("click", function() {
+    let result = window.confirm("Confirm that you want to abort the mission.");
+
+    if (result) {
+        shuttleStatus.innerHTML = "Mission aborted.";
+        shuttleBackground.style.background = "green";
+        shuttleHeight.innerHTML = 0;
+    }
+});
+
+// upButton.addEventListener("click", function() {
+    
+// })
+
 
 
