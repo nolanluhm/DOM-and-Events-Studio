0fdfed51dfaf6e411c1538df21bd2e506b28d320
nolanluhm <nolanluhm@gmail.com>
2020-03-18 19:42:32 -0500

    Updating progress

--- scripts.js
+++ scripts.js
@@ -1,2 +1,18 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+
+window.addEventListener("load", function () {
+    let rocketImg = document.getElementById("rocket");
+    let takeoffButton = document.getElementById("takeoff");
+    let shuttleStatus = document.getElementById("flightStatus")
+});
+
+takeoffButton.addEventListener("click", function() {
+    let result = window.confirm("Confirm that the shuttle is ready for takeoff.");
+
+    if (result) {
+        flightStatus.innerHTML = "Shuttle in flight.”;
+    }
+});
+
+
